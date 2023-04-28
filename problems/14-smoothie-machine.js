4e08b23e77fee8e96c9af379a67a0cd04691d841
Rasheed Lindsey <rasheedlindsey@live.com>
2023-04-28 08:21:12 -0400

    15 solved

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -22,8 +22,28 @@ console.log(smoothie2("pineapple"));
 // prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 ***********************************************************************/
 
-// Your code here
+const smoothieMachine = () => {
+  let base = "I'm having a smoothie with";
 
+  const smoothie = (ingredient) => {
+    base += " " + ingredient;
+    return base
+  };
+  return smoothie
+};
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = smoothieMachine;
