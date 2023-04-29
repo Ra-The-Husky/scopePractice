2de8ae57b9c04d5d38c744a58a435ee13ac29cfd
Rasheed Lindsey <rasheedlindsey@live.com>
2023-04-29 04:00:47 -0400

    18 solved

--- problems/18-planned-intersection.js
+++ problems/18-planned-intersection.js
@@ -15,7 +15,24 @@ console.log(fame(["a", "f", "z", "b"])); // returns [ 'f', 'a' ]
 
 *******************************************************************************/
 
-// Your code here
+const plannedIntersect = (firstArr) => {
+  const sharedArr = []
+  return (secondArr) => {
+   for (let i = 0; i < secondArr.length; i++) {
+    const currEl = secondArr[i]
+    if (firstArr.includes(currEl)) {
+      sharedArr.push(currEl)
+    }
+   }
+   return sharedArr
+  }
+}
+
+let abc = plannedIntersect(["a", "b", "c"]); // returns a function
+console.log(abc(["b", "d", "c"])); // returns [ 'b', 'c' ]
+
+let fame = plannedIntersect(["f", "a", "m", "e"]); // returns a function
+console.log(fame(["a", "f", "z", "b"])); // returns [ 'f', 'a' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 
